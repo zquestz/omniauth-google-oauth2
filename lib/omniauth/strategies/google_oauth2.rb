414c43ef3ffec37d473321f262e80f1e46dda89f
Vlad Shulman <vladshulman@gmail.com>
2017-09-03 16:36:53 -0700

    fixing issue #301 - hd=* should be supported (#303)

--- lib/omniauth/strategies/google_oauth2.rb
+++ lib/omniauth/strategies/google_oauth2.rb
@@ -206,7 +206,7 @@ module OmniAuth
         options.hd = options.hd.call if options.hd.is_a? Proc
         allowed_hosted_domains = Array(options.hd)
 
-        raise CallbackError.new(:invalid_hd, 'Invalid Hosted Domain') unless allowed_hosted_domains.include? @raw_info['hd']
+        raise CallbackError.new(:invalid_hd, 'Invalid Hosted Domain') unless allowed_hosted_domains.include?(@raw_info['hd']) || options.hd == '*'
         true
       end
     end
