d5af158da407e392df5307621fea59e49a273e2b
Vladislav Zhuravlev <warzon131@gmail.com>
2022-09-20 20:57:06 +0300

    Fixed comma in example

--- examples/omni_auth.rb
+++ examples/omni_auth.rb
@@ -8,11 +8,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # Default usage, this will give you offline access and a refresh token
   # using default scopes 'email' and 'profile'
   #
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'email,profile'
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'email, profile'
 
   # Custom redirect_uri
   #
-  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'email,profile', redirect_uri: 'https://localhost:3000/redirect'
+  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'email, profile', redirect_uri: 'https://localhost:3000/redirect'
 
   # Manual setup for offline access with a refresh token.
   #
@@ -21,7 +21,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # Custom scope supporting youtube. If you are customizing scopes, remember
   # to include the default scopes 'email' and 'profile'
   #
-  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'http://gdata.youtube.com,email,profile,plus.me'
+  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'http://gdata.youtube.com, email, profile, plus.me'
 
   # Custom scope for users only using Google for account creation/auth and do not require a refresh token.
   #
@@ -29,7 +29,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   # To include information about people in your circles you must include the 'plus.login' scope.
   #
-  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], skip_friends: false, scope: 'email,profile,plus.login'
+  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], skip_friends: false, scope: 'email, profile, plus.login'
 
   # If you need to acquire whether user picture is a default one or uploaded by user.
   #
